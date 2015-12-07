9dfec62d850f3298e3033742fb8a15b0772cc9d8
Lon Sun <lon.sun@gmail.com>
2015-12-06 21:01:20 -0800

    Add response parsing code and tests.  Also improved some tests.

--- tests/ForteClientTests.php
+++ tests/ForteClientTests.php
@@ -82,8 +82,8 @@ class ForteClientTests extends PHPUnit_Framework_TestCase
 
     $response = new Forte\ForteResponse( $raw_response );
 
-    $this->assertEquals( $response->type, "D" );
-    $this->assertEquals( $response->code, "U19" );
+    $this->assertEquals( $response->result, "D" );
+    $this->assertEquals( $response->result_code, "U19" );
     $this->assertEquals( $response->description, "INVALID TRN" );
     $this->assertEquals( $response->trace_number,
       "4A06A2E2-11EF-484F-AC79-682588B7013F" );
@@ -93,19 +93,92 @@ class ForteClientTests extends PHPUnit_Framework_TestCase
     $raw_response = "pg_response_type=D
       pg_response_code=U19
       pg_total_amount=1.23
+      pg_response_description=INVALID TRN
       pg_trace_number=4A06A2E2-11EF-484F-AC79-682588B7013F
+      test_error_description=MANDITORY FIELD MISSING:pg_merchant_id,MANDITORY FIELD MISSING:ecom_billto_postal_name_first,MANDITORY FIELD MISSING:ecom_billto_postal_name_last
       endofdata";
 
     $expected_result = '{';
     $expected_result .= '"pg_response_type":"D",'; 
     $expected_result .= '"pg_response_code":"U19",'; 
     $expected_result .= '"pg_total_amount":"1.23",'; 
-    $expected_result .= '"pg_trace_number":"4A06A2E2-11EF-484F-AC79-682588B7013F"'; 
+    $expected_result .= '"pg_response_description":"INVALID TRN",'; 
+    $expected_result .= '"pg_trace_number":"4A06A2E2-11EF-484F-AC79-682588B7013F",'; 
+    $expected_result .= '"test_error_description":"MANDITORY FIELD MISSING:pg_merchant_id,MANDITORY FIELD MISSING:ecom_billto_postal_name_first,MANDITORY FIELD MISSING:ecom_billto_postal_name_last"'; 
     $expected_result .= '}';
 
     $this->assertEquals( Forte\ForteResponse::rawResponseToJson( $raw_response ),
       $expected_result );
   }
+
+  public function testIsSuccesful() {
+    # success
+    $raw_response = "
+      pg_response_type=A
+      pg_response_code=A01
+      pg_response_description=APPROVED
+      pg_authorization_code=14025988
+      pg_trace_number=CC0D1E15-628F-46E3-A749-601FB80D1A0D
+      endofdata";
+
+    $response = new Forte\ForteResponse( $raw_response );
+
+    $this->assertEquals( $response->isSuccessful(), true );
+
+    # not successfull
+    $raw_response = "pg_response_type=D
+      pg_response_code=U19
+      pg_total_amount=1.23
+      pg_response_description=INVALID TRN
+      pg_trace_number=4A06A2E2-11EF-484F-AC79-682588B7013F
+      endofdata";
+
+    $response = new Forte\ForteResponse( $raw_response );
+
+    $this->assertEquals( $response->isSuccessful(), false );
+  }
+
+  public function testHasError() {
+    # success
+    $raw_response = "
+      pg_response_type=A
+      pg_response_code=A01
+      pg_response_description=APPROVED
+      pg_authorization_code=14025988
+      pg_trace_number=CC0D1E15-628F-46E3-A749-601FB80D1A0D
+      endofdata";
+
+    $response = new Forte\ForteResponse( $raw_response );
+
+    $this->assertEquals( $response->hasError(), false );
+
+    # not successfull
+    $raw_response = "pg_response_type=D
+      pg_response_code=U19
+      pg_total_amount=1.23
+      pg_response_description=INVALID TRN
+      pg_trace_number=4A06A2E2-11EF-484F-AC79-682588B7013F
+      endofdata";
+
+    $response = new Forte\ForteResponse( $raw_response );
+
+    $this->assertEquals( $response->hasError(), false );
+
+    # has an error
+    $raw_response = "pg_response_type=E
+      pg_response_code=E10
+      pg_response_description=MANDITORY FIELD MISSING:pg_merchant_id,MANDITORY FIELD MISSING:ecom_billto_postal_name_first,MANDITORY FIELD MISSING:ecom_billto_postal_name_last
+      pg_trace_number=E1BC65D5-EEC6-4187-A76E-65F8AE33EB48
+      pg_transaction_type=23
+      pg_total_amount=1.23
+      endofdata
+      ";
+
+    $response = new Forte\ForteResponse( $raw_response );
+
+    $this->assertEquals( $response->hasError(), true );
+  }
+
 
   ##                   ##
   ## integration tests ##
@@ -118,7 +191,7 @@ class ForteClientTests extends PHPUnit_Framework_TestCase
         "payment_amount" => 1.23,
         "account_type" => "C",
         "account_number" => "12345",
-        "routing_number" => "123456789",
+        "routing_number" => "265473812",
         "payee_first_name" => "Bob",
         "payee_last_name" => "Lablaw"
       );
@@ -136,7 +209,7 @@ class ForteClientTests extends PHPUnit_Framework_TestCase
         "payment_amount" => 1.23,
         "account_type" => "C",
         "account_number" => "12345",
-        "routing_number" => "123456789",
+        "routing_number" => "265473812",
         "payee_first_name" => "Bob",
         "payee_last_name" => "Lablaw"
       );
